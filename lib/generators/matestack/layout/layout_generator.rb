8e56808bd22f93ed927f7bc00d2086c49b73f68d
Aaron Contreras <aaronlcaq@gmail.com>
2023-01-18 15:05:23 -0500

    Add Matestack::Ui::Layout Generator

--- lib/generators/matestack/layout/layout_generator.rb
+++ lib/generators/matestack/layout/layout_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators/base'
 require_relative '../constants'
 
 module Matestack
-  class PageGenerator < Rails::Generators::Base
+  class LayoutGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
     argument :name, type: :string
@@ -14,20 +14,20 @@ module Matestack
     include Constants
 
     def setup_generator_config
-      @page_config = {}.tap do |config|
-        config[:template_file_path] = "page#{TEMPLATE_FILE_TYPE}"
+      @layout_config = {}.tap do |config|
+        config[:template_file_path] = "layout#{TEMPLATE_FILE_TYPE}"
         config[:file_path] = "#{MATESTACK_DIRECTORY}/#{name.underscore}#{RUBY_FILE_TYPE}"
         config[:klass] = name.split('/').map(&:camelcase).join('::')
-        config[:base_klass] = if options[:base] && File.exist?(APPLICATION_PAGE_DEFAULT_FILE_PATH)
-                                'ApplicationPage'
+        config[:base_klass] = if options[:base] && File.exist?(APPLICATION_LAYOUT_DEFAULT_FILE_PATH)
+                                'ApplicationLayout'
                               else
-                                'Matestack::Ui::Page'
+                                'Matestack::Ui::Layout'
                               end
       end
     end
 
-    def generate_matestack_page
-      template @page_config[:template_file_path], @page_config[:file_path]
+    def generate_matestack_layout
+      template @layout_config[:template_file_path], @layout_config[:file_path]
     end
   end
 end
